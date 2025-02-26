0e1f319038788f99fbba0a5a782b74fd5502b9e5
tronghunguiux <tronghung.uiux@gmail.com>
2025-02-27 00:15:30 +0700

    update appage install

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Link from "next/link";
+import DefaultLayout from "../template/Template";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>
+          <DefaultLayout showAsideLeft showAsideRight classPage={''}>
+              <h1>Welcome to Pokémon List</h1>
+              <Link href="/pokemon">
+                  <button className="btn btn-outline-primary navigate-btn">Let&apos;s Catch Them All! Pokémon</button>
+              </Link>
+          </DefaultLayout>
+        </h1>
+    </div>
+  );
+};
+
+export default Home;
