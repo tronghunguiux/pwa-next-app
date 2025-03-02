8ba4da273b6c964b4a6df96fad664b5c3ae16b42
tronghunguiux <tronghung.uiux@gmail.com>
2025-03-02 23:50:53 +0700

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 // import '../assets/styles/template.scss'
-// import '@scss/template.scss'
+import '@scss/template.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
